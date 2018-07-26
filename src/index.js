2ac868fa376f47526c7e8fac1de4fdeb03c93ff7
Pnguy55 <pnguy55@gmail.com>
2018-07-26 17:48:48 -0500

    Play with Youtube API.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyCr-JxS8-M6GwCX3eyiJnHdg7QKjOWUVKM'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//Create a new component.  This component should produce
+//some HTML
+
+class App extends Component{
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      //same as this.setState({videos: videos}); for less code
+    });
+  }
+
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+//Take this component's generated HTML and put it
+//on the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ module.exports = {
     extensions: ['', '.js', '.jsx']
   },
   devServer: {
+    port: 8083,
     historyApiFallback: true,
     contentBase: './',
     watchOptions: {
