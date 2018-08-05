735d168b61fad394d444d553a46d5ab8b1befeb5
Pnguy55 <pnguy55@gmail.com>
2018-08-05 00:02:06 -0500

    Finished up the simple youtube app.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -22,20 +23,24 @@ class App extends Component{
                    selectedVideo: null
                  };
 
-    YTSearch({key: API_KEY, term: 'bitcoin'}, (videos) => {
+    this.videoSearch('bitcoin');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
       });
-      //same as this.setState({videos: videos}); for less code
     });
   }
 
-
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term ) }, 300);
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange = {videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
